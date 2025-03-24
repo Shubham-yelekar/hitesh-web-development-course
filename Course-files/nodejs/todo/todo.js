92657f1eb9a7bf5f8c02b1fc05a9d90b6cdb6d53
shubham <shubhamyelekar0@gmail.com>
2025-03-24 18:25:05 +0530

    simple CRUD using file module in node

--- Course-files/nodejs/todo/todo.js
+++ Course-files/nodejs/todo/todo.js
@@ -1,10 +1,11 @@
-const fs = require("fs");
-const filePath = "./tasks.json";
+const fs = require("fs"); // reqires file system modules
+const filePath = "./tasks.json"; // file path
 
 const loadTasks = () => {
   try {
     const dataBuffer = fs.readFileSync(filePath);
     const dataJSON = dataBuffer.toString();
+    console.log(JSON.parse(dataJSON));
     return JSON.parse(dataJSON);
   } catch (error) {
     return [];
@@ -20,7 +21,6 @@ const addTask = (task) => {
   const tasks = loadTasks();
   tasks.push({ task });
   saveTasks(tasks);
-  console.log("Task addded ", task);
 };
 
 const listTasks = () => {
@@ -28,15 +28,30 @@ const listTasks = () => {
   tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
 };
 
-const command = process.argv[2];
-const argument = process.argv[3];
+const removeTask = (taskId)=> {
+  const tasks = loadTasks()
+  const index = taskId - 1
 
-if (command === "add") {
+  if (index < 0 || index >= tasks.length) {
+    console.log("Invalid task number.");
+    return;
+  }
+
+  const removedTask = tasks.splice(index, 1)
+  saveTasks(tasks);
+}
+
+ // command line parameters 
+ // "proceess" process the comand with "argv" property return an array after processing the command
+const command = process.argv[2]; // process the command
+const argument = process.argv[3]; // processes the argument
+
+if (command === "add") {  // Commands or actions 
   addTask(argument);
 } else if (command === "list") {
   listTasks();
 } else if (command === "remove") {
   removeTask(parseInt(argument));
 } else {
-  console.log("Command not found !");
+  console.log("Command not found !"); 
 }
